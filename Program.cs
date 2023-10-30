d0df0906e0ae0df81e4c3ea06bb4cb4df9cbeaa2
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-10-30 10:46:49 +0600

    Задание 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Задание 1
+            //Создайте кортеж User, содержащий имя пользователя Name и массив с текстовой информацией о его пяти любимых блюдах Dishes.
+            //Заполните имя пользователя через ввод в консоль, а информацию о блюдах — в цикле через консоль.
+            (string Name, string[] FavoriteDishes) User;
+            Console.Write("Введите имя: ");
+            User.Name = Console.ReadLine();
+            User.FavoriteDishes = new string[5];
+
+            for (int i = 0; i < User.FavoriteDishes.GetLength(0); i++)
+            {
+                Console.Write("Введите любимое блюдо #{0}: ", i + 1);
+                User.FavoriteDishes[i] = Console.ReadLine();
+            }
+
         }
     }
 }
