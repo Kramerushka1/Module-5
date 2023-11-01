d03a5082853c601ffb01a9b494b21442f60314d9
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-01 10:25:11 +0600

    Задание 5.1.6

--- Program.cs
+++ Program.cs
@@ -2,61 +2,49 @@
 {
     internal class Program
     {
-        //Задание 5.1.5
-        //Напишите программу, которая:
-        // - В цикле вызывает метод ShowColor()
-        // - Записывает его значение в массив из трех цветов favcolors
-        // - Отображает значения этого массива
+        //Задание 5.1.6
+        //Модифицируйте метод из скринкаста:  после ввода массива с клавиатуры необходимо отсортировать массив и вывести его на экран.
 
-        static string ShowColor()
+        static int[] ArraySort(int[] arr)
         {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-            var color = Console.ReadLine();
-
-            switch (color)
+            int value = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is red!");
-                    return color;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    return color;
-
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    return color;
-
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Your color is yellow!");
-                    return color;
+                for (int j = i + 1; j < arr.GetLength(0); j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        value = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = value;
+                    }
+                }
             }
+            
+            return arr;
+        }
+        static int[] GetArrayFromConsole()
+        {
+            int[] result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            result = ArraySort(result);
+
+            return result;
         }
 
         public static void Main(string[] args)
         {
-            string[] favColors = new string[3];
-            for (int i = 0; i < favColors.Length; i++)
-            {
-                favColors[i] = ShowColor();
-            }
+            int[] arr = GetArrayFromConsole();
 
-            Console.WriteLine("Любимые цвета: ");
-            foreach (string favColor in favColors)
+            foreach (int i in arr)
             {
-                Console.WriteLine(favColor);
+                Console.WriteLine(i);
             }
 
         }
