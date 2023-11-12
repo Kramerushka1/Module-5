256a0ae063893c6d4a13ad6833080a088e863e7d
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-12 19:40:40 +0600

    Задание 5.5.4 (another)

--- Program.cs
+++ Program.cs
@@ -31,58 +31,12 @@ namespace Module_5
                 modif = modif.Remove(0, 2);
             }
 
-            ChangeColor(deep);
+            Console.BackgroundColor = (ConsoleColor)deep;
             Console.WriteLine("..." + modif);
 
             if (deep > 1)
             {
                 Echo(modif, deep - 1);
-            }
-        }
-
-        static void ChangeColor(int color)
-        {
-            switch (color)
-            {
-                case 1:
-                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
-
-                case 2:
-                    Console.BackgroundColor = ConsoleColor.DarkBlue;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
-
-                case 3:
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    break;
-
-                case 4:
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    break;
-
-                case 5:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    break;
-
-                case 6:
-                    Console.BackgroundColor = ConsoleColor.DarkYellow;
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    break;
-
-                case 7:
-                    Console.BackgroundColor = ConsoleColor.DarkRed;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
-
-                default:
-                    Console.BackgroundColor = ConsoleColor.Black;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
             }
         }
     }
