6eb70ab028b72eb6480d4e81bbfc6d2a097b913b
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-12 19:24:13 +0600

    Задание 5.5.3

--- Program.cs
+++ Program.cs
@@ -6,79 +6,36 @@ namespace Module_5
 {
     internal class Program
     {
-        //Задание 5.3.13
-        //Используйте код метода SortArray. Сейчас этот метод сортирует массив по возрастанию значения.
-        //Создайте методы SortArrayDesc и SortArrayAsc — сортировка массива от большего к меньшему и сортировка массива от меньшего к большему.
-        //
-        //Метод SortArray модифицируйте так, чтобы он вызвал оба этих метода.
-        //Результаты методов SortArrayAsc и SortArrayDesc должны представлять собой массивы.
-        //
-        //Метод SortArray должен иметь один входной параметр array и два выходных: sorteddesc и sorted asc.
-
-
+        //Задание 5.5.3
+        //Залейте упражнение в свой репозиторий в Git.
 
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Напишите что-то");
+            var phrase = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(phrase, deep);
+
+            Console.ReadKey();
         }
-
-        static int[] SortArrayDesc(int[] array)
+        static void Echo(string saidword, int deep)
         {
-            int value;
+            var modif = saidword;
 
-            for (int i = 0; i < array.Length; i++)
+            if (modif.Length > 2)
             {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[i] < array[j])
-                    {
-                        value = array[i];
-                        array[i] = array[j];
-                        array[j] = value;
-                    }
-                }
+                modif = modif.Remove(0, 2);
             }
 
-            return array;
-        }
+            Console.WriteLine("..." + modif);
 
-        static int[] SortArrayAsc(int[] array)
-        {
-            int value;
-
-            for (int i = 0; i < array.Length; i++)
+            if (deep > 1)
             {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[i] > array[j])
-                    {
-                        value = array[i];
-                        array[i] = array[j];
-                        array[j] = value;
-                    }
-                }
+                Echo(modif, deep - 1);
             }
-
-            return array;
         }
-
-        static void SortArray(in int[] array, out int[] sorteddesc, out int[] sortedasc)
-        {
-            sorteddesc = SortArrayDesc(array);
-            sortedasc = SortArrayAsc(array);
-        }
-
-        static int[] GetArrayFromConsole()
-        {
-            int[] array = new int[4];
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write("Введите {0} число: ", i + 1);
-                array[i] = int.Parse(Console.ReadLine());
-            }
-
-            return array;
-        }
-
     }
 }
