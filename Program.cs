739a13f23496663509df89e3b22ab6c9b9970f9b
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-12 19:36:44 +0600

    Задание 5.5.4

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace Module_5
 {
     internal class Program
     {
-        //Задание 5.5.3
-        //Залейте упражнение в свой репозиторий в Git.
+        //Задание 5.5.4
+        //Пусть каждая строка окрашивается в новый цвет, связанный с глубиной deep.
 
         public static void Main(string[] args)
         {
@@ -21,6 +21,7 @@ namespace Module_5
 
             Console.ReadKey();
         }
+
         static void Echo(string saidword, int deep)
         {
             var modif = saidword;
@@ -30,11 +31,58 @@ namespace Module_5
                 modif = modif.Remove(0, 2);
             }
 
+            ChangeColor(deep);
             Console.WriteLine("..." + modif);
 
             if (deep > 1)
             {
                 Echo(modif, deep - 1);
+            }
+        }
+
+        static void ChangeColor(int color)
+        {
+            switch (color)
+            {
+                case 1:
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+
+                case 2:
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+
+                case 3:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+
+                case 4:
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+
+                case 5:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+
+                case 6:
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+
+                case 7:
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
             }
         }
     }
